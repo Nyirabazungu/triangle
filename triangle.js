c4a1e9e5b9cdb5da06ad4fd92776190738fbd227
Nyirabazungu <Rutagarama1>
2018-12-13 18:40:53 +0200

    add variables

---
+++ triangle.js
@@ -0,0 +1,12 @@
+function triangle(){
+    var triangle=documents.getElementById("side1","side2","side3").Value;
+    alert(triangle);
+}
+var triangle=("side1","side2","side3");
+var side1 = parseInt("Enter number");
+var side2 =parseInt("Enter number");
+var side3 =parseInt("Enter number");
+
+
+
+
