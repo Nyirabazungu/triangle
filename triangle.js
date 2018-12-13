dda11410c3ea952177fc3787f99ba720aa9e392d
Nyirabazungu <Rutagarama1>
2018-12-13 18:57:37 +0200

    add condition

--- triangle.js
+++ triangle.js
@@ -1,11 +1,25 @@
-function triangle(){
-    var triangle=documents.getElementById("side1","side2","side3").Value;
-    alert(triangle);
-}
+function myFunction() {
+    var firstString =document.getElementById("myNumber").value;
+    alert(firstString);
+    }
+var triangle = 180;{
 var triangle=("side1","side2","side3");
 var side1 = parseInt("Enter number");
 var side2 =parseInt("Enter number");
 var side3 =parseInt("Enter number");
+}
+if( side1 == side2&& side1==side3 &&side2==side3);{
+console.log("Equilateral triangle");
+}
+elseif(side1==side2||side1==side3||side2==side3){
+    console.log("isoceles triangle");
+}
+elseif(side1+side2>side3&&side1+side3>side2&&side2+side3>side1);{
+    console.log("scalene triangle");
+    }
+else{
+    console.log("not triangle");
+}
 
 
 
