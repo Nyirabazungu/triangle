b961d4e838adb3598161202be895812a79a50b54
Nyirabazungu <Rutagarama1>
2018-12-14 15:43:32 +0200

    pull README

--- triangle.js
+++ triangle.js
@@ -1,15 +1,15 @@
 function myFuction() {
-  var side1=parseInt(document.getElementById("side1").value);
-  var side2=parseInt(document.getElementById("side2").value);
-  var side3=parseInt(document.getElementById("side3").value);
+  var side1 = parseInt(document.getElementById("side1").value);
+  var side2 = parseInt(document.getElementById("side2").value);
+  var side3 = parseInt(document.getElementById("side3").value);
 
-  if (side1===side2 && side1===side3 && side2===side3){
+  if (side1 === side2 && side1 === side3 && side2 === side3){
     alert("equilateral triangle");
     }
-    else if (side1===side2||side1<side3||side2<ide3){
+    else if (side1 === side2 || side1<side3 || side2<ide3){
     alert("isoceles triangle");
     }
-    else if (side1+side2 !==side3 ||side1 + side3!==side2 ||side2+side3!==side1) {
+    else if ((side1+side2) <=side3 || (side1 + side3)<= side2 || (side2+side3)!== side1) {
     alert("Scalene triangle");
     }
     alert("Not a triangle");
