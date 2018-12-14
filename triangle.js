fde73254fbe3d6ee9883756437635c8ef23ab2d8
Nyirabazungu <Rutagarama1>
2018-12-14 14:51:09 +0200

    add image

--- triangle.js
+++ triangle.js
@@ -1,23 +1,17 @@
-function myFuction () {
-    var side1 =parseInt(document.getElementById("side1").value);
-    var side2 =parseInt(document.getElementById("side2").value);
-    var side3 =parseInt(document.getElementById("side3").value);
+function myFuction() {
+  var side1=parseInt(document.getElementById("side1").value);
+  var side2=parseInt(document.getElementById("side2").value);
+  var side3=parseInt(document.getElementById("side3").value);
 
-if (side1===side2 && side1===side3 && side2===side3){
-   alert("equilateral triangle");
-
-}
-
-else if (side1===side2||side1<side3||side2<side3){
-  alert("isoceles triangle");
-
-}
-
-else if (side1+side2 !== side3 ||side1+ side3!==side2 ||side2+side3!==side1){
-   alert("Scalene triangle");
-
-}
-else {
-  alert("Not a triangle");
-}
+  if (side1===side2 && side1===side3 && side2===side3){
+    alert("equilateral triangle");
+    }
+    else if (side1===side2||side1<side3||side2<ide3){
+    alert("isoceles triangle");
+    }
+    else if (side1+side2 !==side3 ||side1 + side3!==side2 ||side2+side3!==side1) {
+    alert("Scalene triangle");
+    }
+    alert("Not a triangle");
+  }
 }
