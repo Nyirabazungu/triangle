07b3689b2f50f84a33281c571529b9ac86e05a84
Nyirabazungu <Rutagarama1>
2018-12-17 10:25:07 +0200

    add break

--- triangle.js
+++ triangle.js
@@ -1,19 +1,19 @@
-function myFuction(){
-  var side1 = parseInt(document.getElementById("S1").value);
-  var side2 = parseInt(document.getElementById("S2").value);
-  var side3 = parseInt(document.getElementById("S3").value);
+function mytriangle(){
+  var side1=parseInt(document.getElementById("S1").value);
+  var side2=parseInt(document.getElementById("S2").value);
+  var side3=parseInt(document.getElementById("S3").value);
+var triangle=180;
+  if ([side1 === side2 && side1 === side3]){
+    console.log(alert("equilateral triangle"));
+    }
+    else if ([side1 === side2 || side1<side3 || side2<ide3]){
+    console.log(alert("isoceles triangle"));
+    }
+    else if ([(side1+side2) <=side3 || (side1 + side3)<= side2 || (side2+side3)!== side1]) {
+    console.log(alert("Scalene triangle"));
+    }
+    console.log(alert("Not a triangle"));{
 
-  if (side1 === side2 && side1 === side3){
-    alert("equilateral triangle");
-    }
-    else if (side1 === side2 || side1<side3 || side2<ide3){
-    alert("isoceles triangle");
-    }
-    else if ((side1+side2) <=side3 || (side1 + side3)<= side2 || (side2+side3)!== side1) {
-    alert("Scalene triangle");
-    }
-    alert("Not a triangle");{
-      
     }
   }
 
