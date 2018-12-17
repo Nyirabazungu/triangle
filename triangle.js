b670467b200169490b7dcf9e857732cc1636ebb8
Nyirabazungu <Rutagarama1>
2018-12-17 18:54:11 +0200

    add array

--- triangle.js
+++ triangle.js
@@ -3,20 +3,20 @@ function mytriangle(){
   var side2=parseInt(document.getElementById("S2").value);
   var side3=parseInt(document.getElementById("S3").value);
 
-  if (side1+side2>side3&&side1+side3>side2&&side2+side3>side1)
+  if ([side1+side2]>[side3]&&[side1+side3]>[side2]&&[side2+side3]>[side1])
   {
-  if(side1===side2&&side2===side3&&side1===side3){
+  if([side1===side2]&&[side2===side3]&&[side1===side3]){
     alert("equilateral triangle");
     }
     else if (side1 === side2 || side1===side3 ||side2===side3){
     alert("isoceles triangle");
     }
-    else if (side1+side2 > side3 && side1+ side3>side2 && side2+side3>side1) {
+    else if (side1+side2 > side3&& side1+ side3>side2 && side2+side3>[side1]){
     alert("Scalene triangle");
     }
     }
     else{
-    alert("No triangle")
+    alert("Not a triangle")
 
     }
   }
