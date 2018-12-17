50044812636ac16a2a04c4b10c92a58cecda13d8
Nyirabazungu <Rutagarama1>
2018-12-17 12:02:31 +0200

    add div

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # triangle
+
+this project 
+
+## setup used
+
+-visual studio code
+-google chrome
+-terminal
+
+##contributors
+
+-Nyirabazungu Valentine < nyanki6@gmail.com>
+
+ ##link to live site on GitHub pages
+
+  https://github.com/Nyirabazungu/triangle
+
+## lisence &copyright
+
+(c) Nyirabazungu Valentine,2018
+
--- triangle.js
+++ triangle.js
@@ -2,17 +2,20 @@ function mytriangle(){
   var side1=parseInt(document.getElementById("S1").value);
   var side2=parseInt(document.getElementById("S2").value);
   var side3=parseInt(document.getElementById("S3").value);
-var triangle=180;
-  if ([side1 === side2 && side1 === side3]){
-    console.log(alert("equilateral triangle"));
+  if (side1+side2>side3&&side1+side3>side2&&side2+side3>side1)
+  {
+  if(side1===side2&&side2===side3&&side1===side3){
+    alert("equilateral triangle");
     }
-    else if ([side1 === side2 || side1<side3 || side2<ide3]){
-    console.log(alert("isoceles triangle"));
+    else if (side1 === side2 || side1===side3 ||side2===side3){
+    alert("isoceles triangle");
     }
-    else if ([(side1+side2) <=side3 || (side1 + side3)<= side2 || (side2+side3)!== side1]) {
-    console.log(alert("Scalene triangle"));
+    else if (side1+side2 > side3 && side1+ side3>side2 && side2+side3>side1) {
+    alert("Scalene triangle");
     }
-    console.log(alert("Not a triangle"));{
+    }
+    else{
+    alert("Not a triangle");
 
     }
   }
